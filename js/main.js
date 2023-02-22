6b93572be4332b00948b594f882e658c2ade0d25
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-22 16:22:51 +0100

    different age sale price

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// - Creo altre due variabili per gli sconti a minorenni (0.8) e over 65 (0.6);
-// - SE il passeggero è minorenne, applico la variabile dello sconto del 20% (prezzo intero * 0.8);
-// - SE il passeggero è over 65, applico invece la variabile dello sconto del 40% (prezzo intero * 0.6);
 // - Inserisco un bottone che, al click, esegue una funzione anonima che mi restituisce il valore totale del biglietto in console;
 
 // - Realizzo un form completo in cui l'utente può inserire tutti i dati e visualizzare il prezzo finale con relativo biglietto;
@@ -10,7 +7,6 @@
 
 // Tramite l'utilizzo di due input, chiedo all'utente quanti chilometri deve percorrere il passeggero e quanti anni ha
 // Inserisco i due valori in altrettante variabili e, dal momento che gli input vengono forniti in formato stringa, li trasformo in numeri
-
 const passengerDistance = Number(document.getElementById('passenger-distance').value);
 const passengerAge = Number(document.getElementById('passenger-age').value);
 
@@ -18,6 +14,18 @@ const passengerAge = Number(document.getElementById('passenger-age').value);
 const basePrice = 0.21;
 
 // - Moltiplico la variabile del prezzo per il numero di chilometri da percorrere e ottengo così il prezzo intero del biglietto;
-const ticketPrice = basePrice * passengerDistance;
+let ticketPrice = basePrice * passengerDistance;
+
+// - Creo altre due variabili per gli sconti a minorenni (0.8) e over 65 (0.6);
+const minorSale = 0.8;
+const overSale = 0.6;
+
+// - SE il passeggero è minorenne, applico la variabile dello sconto del 20% (prezzo intero * 0.8);
+// - SE il passeggero è over 65, applico invece la variabile dello sconto del 40% (prezzo intero * 0.6);
+if (passengerAge < 18) {
+  ticketPrice *= minorSale;
+} else if (passengerAge >= 65) {
+  ticketPrice *= overSale;
+}
 
 console.log(ticketPrice);
