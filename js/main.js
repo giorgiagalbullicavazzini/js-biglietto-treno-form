c1d85235871912e633a99523d76ef6d15cfa8df8
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-22 16:41:56 +0100

    fix typo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// - Realizzo un form completo in cui l'utente può inserire tutti i dati e visualizzare il prezzo finale con relativo biglietto;
+// Realizzo un form completo in cui l'utente può inserire tutti i dati e visualizzare il prezzo finale con relativo biglietto;
 
 'use strict';
 
-// - Il valore base del biglietto è di €0.21 al km, quindi assegno questo valore a un'altra variabile;
+// Il valore base del biglietto è di €0.21 al km, quindi assegno questo valore a un'altra variabile;
 const basePrice = 0.21;
 
-// - Creo altre due variabili per gli sconti a minorenni (0.8) e over 65 (0.6);
+// Creo altre due variabili per gli sconti a minorenni (0.8) e over 65 (0.6);
 const minorSale = 0.8;
 const overSale = 0.6;
 
-// - Inserisco un bottone che, al click, esegue una funzione anonima che mi restituisce il valore totale del biglietto in console;
+// Inserisco un bottone che, al click, esegue una funzione anonima che mi restituisce il valore totale del biglietto in console;
 const button = document.getElementById('button');
 
 button.addEventListener('click',
@@ -19,17 +19,17 @@ button.addEventListener('click',
     const passengerDistance = Number(document.getElementById('passenger-distance').value);
     const passengerAge = Number(document.getElementById('passenger-age').value);
 
-    // - Moltiplico la variabile del prezzo per il numero di chilometri da percorrere e ottengo così il prezzo intero del biglietto;
+    // Moltiplico la variabile del prezzo per il numero di chilometri da percorrere e ottengo così il prezzo intero del biglietto;
     let ticketPrice = basePrice * passengerDistance;
 
-    // - SE il passeggero è minorenne, applico la variabile dello sconto del 20% (prezzo intero * 0.8);
-    // - SE il passeggero è over 65, applico invece la variabile dello sconto del 40% (prezzo intero * 0.6);
+    // SE il passeggero è minorenne, applico la variabile dello sconto del 20% (prezzo intero * 0.8);
+    // SE il passeggero è over 65, applico invece la variabile dello sconto del 40% (prezzo intero * 0.6);
     if (passengerAge < 18) {
       ticketPrice *= minorSale;
     } else if (passengerAge >= 65) {
       ticketPrice *= overSale;
     };
 
-    // - Per fornire un output in forma umana, il numero ricavato dovrà contenere un massimo di due decimali che indicheranno i centesimi da pagare per il biglietto.
+    // Per fornire un output in forma umana, il numero ricavato dovrà contenere un massimo di due decimali che indicheranno i centesimi da pagare per il biglietto.
     console.log(passengerDistance, passengerAge, ticketPrice.toFixed(2));
 });
